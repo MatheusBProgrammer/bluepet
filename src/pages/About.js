36c30460bfce848ea3a9f214ba75f7a5fe12059d
Matheus Barreto <matheusbarretoprogramador@gmail.com>
2023-11-22 15:53:06 -0300

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import styles from "./About.module.css";
+
 function About() {
-  return <div className={styles.container}>Sobre</div>;
+  const whatsappNumber = "+5588999999999";
+  const whatsappMessage = encodeURIComponent(
+    "Olá, gostaria de entrar em contato para tirar uma dúvida."
+  );
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Sobre Nós</h1>
+      <p className={styles.description}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        mauris vel ante lacinia, vel bibendum sapien bibendum. Nullam
+        condimentum, velit vel malesuada tincidunt, velit enim tincidunt mauris,
+        vel bibendum sapien bibendum.
+      </p>
+      <p className={styles.description}>
+        Praesent euismod, nisl vel aliquam ultricies, elit ex lacinia augue,
+        eget bibendum sapien sapien vel nunc. Sed vel magna vel sapien
+        ullamcorper bibendum. Nullam vel sapien vel sapien bibendum ullamcorper.
+        Sed vel magna vel sapien ullamcorper bibendum.
+      </p>
+      <div className={styles.iconContainer}>
+        <a href="https://www.instagram.com/bluepetpb/" className={styles.icon}>
+          <FaInstagram />
+        </a>
+        <a
+          href={`https://wa.me/${whatsappNumber}?text=${whatsappMessage}`}
+          className={styles.icon}
+        >
+          <FaWhatsapp />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./Products.module.css";
 
 function Products() {
-  return <div>products</div>;
+  return (
+    <div>
+      <div className={styles.alert}>
+        <span className={styles.icon}>!</span>
+        <span>ESTA PÁGINA ESTÁ EM DESENVOLVIMENTO </span>
+        <span className={styles.icon}>!</span>
+      </div>
+    </div>
+  );
 }
 
 export default Products;
